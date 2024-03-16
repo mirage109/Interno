338361f4ae765494e4c36268eb712cca8f3987d9
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-16 17:14:06 +0100

    feat testimonials

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-const navTriggerBtn = document.querySelector('#nav_triger_btn');
-const navMenu = document.querySelector('#nav_menu');
+const navTriggerBtn = document.querySelector("#nav_triger_btn");
+const navMenu = document.querySelector("#nav_menu");
 
-navTriggerBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('nav-is-open');
+navTriggerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-is-open");
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+    breakpoints: {
+        320: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        960: {
+        slidesPerView: 3,
+        },
+    },
 });
