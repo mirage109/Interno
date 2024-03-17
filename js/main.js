790520245a8f2e19275b6a3d7e37c83272e89283
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-17 11:05:40 +0100

    feat animation

--- js/main.js
+++ js/main.js
@@ -13,15 +13,50 @@ const swiper = new Swiper(".swiper", {
   },
   slidesPerView: 3,
   spaceBetween: 20,
-    breakpoints: {
-        320: {
-        slidesPerView: 1,
-        },
-        960: {
-        slidesPerView: 2,
-        },
-        1200: {
-        slidesPerView: 3,
-        },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
 });
+
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  delay: 600,
+});
+
+sr.reveal(".hero__text", { origin: "top" });
+sr.reveal(".steps__step", { distance: "100px", interval: 100 });
+
+sr.reveal(".about__text", { origin: "left" });
+sr.reveal(".about__img", { origin: "right", delay: 800 });
+
+sr.reveal(".testimonials__bg", { delay: 800 });
+sr.reveal(".testimonial__title");
+sr.reveal(".testimonial__slider", { delay: 1000 });
+
+sr.reveal(".brands__img", { delay: 600, distance: "100px", interval: 100});
+
+sr.reveal('.work__title')
+sr.reveal('.work__subtitle', { delay: 600})
+sr.reveal('.work__grid', { delay: 1000})
+
+sr.reveal('.stats')
+sr.reveal('.stats__item', { interval: 100, distance: '100px'})
+
+sr.reveal('.news__title')
+sr.reveal('.news__subtitle', { delay: 600})
+sr.reveal('.news__item', { interval: 100, distance: '100px', delay: 1000})
+
+sr.reveal('.contact__container')
+sr.reveal('.contact__text', { delay: 600})
+
+sr.reveal('.footer__item', { interval: 100, distance: '100px'})
